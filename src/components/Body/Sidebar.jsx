6e80fab46e379ef54e4e9ef8923dae52bc4dba04
Changmini <cml039465@gmail.com>
2023-08-23 00:15:53 +0900

    css 수정..

--- src/components/Body/Sidebar.jsx
+++ src/components/Body/Sidebar.jsx
@@ -3,24 +3,34 @@ import { useState } from 'react';
 
 export default function MainPage() {
 
-    function testAction() {
-        console.log("good");
+    function testAction(e) {
+        let p  = e.target;
+        let textUnderbar = document.querySelector("#text-underbar");
+        if (!textUnderbar) return ;
+        if (textUnderbar.style.display !== "block") {
+            textUnderbar.style.display = "block";
+        }
+        // textUnderbar.style.width = (p.offsetWidth - 5) + "px";
+        textUnderbar.style.width = 6 + "rem";
+        textUnderbar.style.top = (p.offsetTop + p.offsetHeight) + "px";
+        textUnderbar.style.left = p.offsetLeft + "px";
     }
 
     return (
         <div className='sidebar'>
             <p>목 록</p>
             <section>
+                <div id='text-underbar'></div>
                 <ul>
                     <li onClick={testAction}>
                         <i className="fa-solid fa-calendar-days"></i>
                         <span>달력</span>
                     </li>
-                    <li>
+                    <li onClick={testAction}>
                         <i className="fa-solid fa-pen-to-square"></i>
                         <span>연대표</span>
                     </li>
-                    <li>
+                    <li onClick={testAction}>
                         <i className="fa-solid fa-chart-column"></i>
                         <span>분석표</span>
                     </li>
