567ecdd14a4bcc291b9d4b94d1933a072207cc26
Changmini <cml039465@gmail.com>
2024-10-15 21:44:58 +0900

    aws 물리주소 재세팅

--- src/common.js
+++ src/common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_PROTOCOL = "http://";
 const API_HOST = "localhost";
-// const API_HOST = "ec2-43-203-220-226.ap-northeast-2.compute.amazonaws.com";
+// const API_HOST = "43.203.220.226/";
 const API_PATHNAME = "/mood"
 const API_ROOT = `${API_PROTOCOL}${API_HOST}${API_PATHNAME}`;
 const methods = {
